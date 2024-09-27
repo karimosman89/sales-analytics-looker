29b5e70618d94fcddc6c9eedee6d791b609b3cf7
Karim Osman <60973591+karimosman89@users.noreply.github.com>
2024-09-27 15:14:16 +0200

    Update data_preprocessing.py

--- src/data_preprocessing.py
+++ src/data_preprocessing.py
@@ -1,1 +1,9 @@
+import pandas as pd
+
+def preprocess_sales_data(data):
+    data['Order Date'] = pd.to_datetime(data['Order Date'])
+    data['Year'] = data['Order Date'].dt.year
+    data['Month'] = data['Order Date'].dt.month
+    data['Sales'] = data['Sales'].apply(lambda x: max(x, 0))  # Clean negative sales
+    return data
 
